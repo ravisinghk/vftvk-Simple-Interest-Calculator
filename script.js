1a7865f4f6bd7a50af2843c989346b698d11200d
Ravisingh Kushwah <ravisinghkushwah27@gmail.com>
2021-09-13 12:02:22 +0530

    Changes by Ravi

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+function range() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value+`%`;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = document.getElementById("principal").value;
+    let years = document.getElementById("years").value;
+    let rate = document.getElementById("rate").value;  
+    let interest = principal*years*rate/100;
+    let year = new Date().getFullYear()+parseInt(years);
+
+
+    if (principal <= 0 ) {
+        alert("Enter a Positive Number ");
+        document.getElementById("principal").focus();
+        return false;
+      }
+
+    document.getElementById("result").innerHTML=`If you deposit <span style="background-color:yellow">${principal}</span>,<br>
+                                                at an interest rate of <span style="background-color:yellow">${rate}%</span><br>
+                                                You will receive an amount of <span style="background-color:yellow">${interest}</span>,<br>
+                                                in the year <span style="background-color:yellow">${year}</span><br>`;
+
 }
-        
+
+let c = document.querySelector(".smalldiv").children;
+
+Array.from(c).forEach(element => {
+    console.log(element);
+    element.style.margin = "10px 5px 10px 0px";
+});
+
+    
